e732b46b2c3b6c4efe6cf1f7b2fefe9fead4d92b
JackyWang2014 <339847016@qq.com>
2016-04-16 10:45:14 +0800

    Bubble_sort

--- Bubble_sort/main.c
+++ Bubble_sort/main.c
@@ -10,5 +10,27 @@
 
 int main(int argc, const char * argv[]) {
 
+    
+    int a[10] = {5,1,3,9,7,6,4,2,0,8};
+    
+    for (int i = 0; i<10; i++) {
+        printf("%d\n",a[i]);
+    }
+    
+    for (int i = 0; i < 10-1; i++) {
+        for (int j = 0; j<10-i-1; j++) {
+            if (a[j]>a[j+1]) {
+                int tmp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = tmp;
+            }
+        }
+    }
+    
+    
+    for (int i = 0; i<10; i++) {
+        printf("%d\n",a[i]);
+    }
+    
     return 0;
 }
